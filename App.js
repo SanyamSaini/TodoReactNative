a97a6150d86a7eb7f8bbcc7c0c446913ad6ccf69
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-04 01:52:32 +0530

    Added nested View elements, state for list and flat list to show the list.

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text : 'buy Coffee', key : '1'},
+    {text : 'Create an App', key : '2'},
+    {text : 'play on the switch', key : '3'}
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Header*/}
+      <View style={styles.content}>
+        {/*Content*/}
+        <View style={styles.list}>
+          {/*List*/}
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
